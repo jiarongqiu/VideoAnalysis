9f9969c7dbc8b94177e713aee109e6e18a2eebe0
charlie818 <jiarong_qiu@sina.com>
2017-12-08 10:23:02 +0800

    update

--- dataProcess/videoConverter.py
+++ dataProcess/videoConverter.py
@@ -5,32 +5,77 @@ def get_dir_list(path, _except=None):
     return [os.path.join(path, x) for x in os.listdir(path) if os.path.isdir(os.path.join(path, x)) and x != _except]
 
 
-def get_file_list(path, _except=None):
-    return [os.path.join(path, x) for x in os.listdir(path) if os.path.isfile(os.path.join(path, x)) and x != _except]
+def get_file_list(path, _except=None, sort=False):
+    if sort:
+        return sorted(
+            [os.path.join(path, x) for x in os.listdir(path) if os.path.isfile(os.path.join(path, x)) and x != _except])
+    else:
+        return [os.path.join(path, x) for x in os.listdir(path) if
+                os.path.isfile(os.path.join(path, x)) and x != _except]
 
-def image2list():
-    pass
+
+def image2list(path, ratio=0.8):
+    dataset = os.path.split(path)[-1].lower()
+    print(dataset)
+    if dataset not in ("ucf-11","ucf-101"):
+        return
+    label_file = open("../data/" + dataset + "_labels.txt", 'w')
+    train_file = open("../data/" + dataset + "_train_list.txt", 'w')
+    test_file = open("../data/" + dataset + "_test_list.txt", 'w')
+    if dataset == "ucf-11":
+        cnt = 0
+        for category in get_dir_list(path):
+            label_file.write(os.path.split(category)[-1] + '\t' + str(cnt) + '\n')
+            length = 0
+            for videos in get_dir_list(category, "Annotation"):
+                length += len(get_dir_list(videos))
+            cnt2 = 0
+            for videos in get_dir_list(category, "Annotation"):
+                for clip in get_dir_list(videos):
+                    if cnt2 < length * ratio:
+                        train_file.write(clip + '\t' + str(cnt) + '\n')
+                    else:
+                        test_file.write(clip + '\t' + str(cnt) + '\n')
+                    cnt2 += 1
+            cnt += 1
+    elif dataset =='ucf-101':
+        cnt = 0
+        for category in get_dir_list(path):
+            label_file.write(os.path.split(category)[-1] + '\t' + str(cnt) + '\n')
+            length = len(get_dir_list(category))
+            cnt2 = 0
+            for clip in get_dir_list(category):
+                if cnt2 < length * ratio:
+                    train_file.write(clip + '\t' + str(cnt) + '\n')
+                else:
+                    test_file.write(clip + '\t' + str(cnt) + '\n')
+                cnt2 += 1
+            cnt += 1
+
+
 
 # generate images for all videos
-def video2image(path, frame=5):
-    dataset = os.path.split(path)[-1]
-    if dataset not in ("UCF-11"):
+def video2image(path, frame=4):
+    dataset = os.path.split(path)[-1].lower()
+    if dataset not in ("ucf-11",'ucf-101'):
         return
-    if dataset == "UCF-11":
-        for folder in get_dir_list(path):
-            print folder
-            for subfolder in get_dir_list(folder, "Annotation"):
-                for file in get_file_list(subfolder):
-                    one_video2image(file, frame)
-            break
+    if dataset == "ucf-11":
+        for category in get_dir_list(path):
+            for videos in get_dir_list(category, "Annotation"):
+                for clip in get_file_list(videos):
+                    one_video2image(clip, frame)
+    elif dataset=='ucf-101':
+        for category in get_dir_list(path):
+            for clip in get_file_list(category):
+                one_video2image(clip, frame)
 
 
-def one_video2image(infile, frame=5):
+def one_video2image(infile, frame=5, crop_size=128):
     folder = os.path.splitext(infile)[0]
     # print folder
     os.system("rm -rf %s" % folder)
     os.system("mkdir %s" % folder)
-    str = "ffmpeg -i %s -vf fps=%d %s" % (infile, frame, folder) + "/%05d.jpg"
+    str = "ffmpeg -i %s -s %d*%d -vf fps=%d %s" % (infile, crop_size, crop_size, frame, folder) + "/%05d.jpg"
     # print str
     os.system(str)
 
@@ -38,5 +83,10 @@ def one_video2image(infile, frame=5):
 if __name__ == '__main__':
     infile = "/home/charlie/Desktop/dataset/UCF-11/basketball/v_shooting_01/v_shooting_01_01.avi"
     outfile = "/home/charlie/Desktop/1.jpg"
+
+    # print get_file_list("/home/charlie/Desktop/dataset/UCF-11/volleyball_spiking/v_spiking_14/v_spiking_14_04")
     # one_video2image(infile)
-    video2image("/home/charlie/Desktop/dataset/UCF-11")
+    # video2image("/home/charlie/Desktop/dataset/UCF-11")
+    # image2list("/home/charlie/Desktop/dataset/UCF-11")
+    # video2image("/home/charlie/Desktop/dataset/UCF-101")
+    image2list("/home/charlie/Desktop/dataset/UCF-101")
--- train.py
+++ train.py
@@ -1,1 +1,167 @@
-import  tensorflow as tf
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Trains and Evaluates the MNIST network using a feed dictionary."""
+# pylint: disable=missing-docstring
+import os
+import time
+import numpy
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+from data import dataReader
+from model import C3D
+
+# Basic model parameters as external flags.
+flags = tf.app.flags
+# flags.DEFINE_float('learning_rate', 0.0, 'Initial learning rate.')
+flags.DEFINE_integer('max_steps', 500, 'Number of steps to run trainer.')
+flags.DEFINE_integer('batch_size', 10, 'Batch size.')
+FLAGS = flags.FLAGS
+MOVING_AVERAGE_DECAY = 0.9999
+model_save_dir = './out'
+NUM_FRAMES_PER_CLIP = 16
+CROP_SIZE = 128
+CHANNELS = 3
+BATCH_SIZE = 16
+
+
+def placeholder_inputs():
+    images_placeholder = tf.placeholder(tf.float32, shape=(BATCH_SIZE,NUM_FRAMES_PER_CLIP,CROP_SIZE,CROP_SIZE,CHANNELS))
+    labels_placeholder = tf.placeholder(tf.int64, shape=(BATCH_SIZE))
+    return images_placeholder, labels_placeholder
+
+
+def get_loss_accuracy(logit,labels):
+    pred=tf.argmax(logit, 1)
+    correct_pred = tf.equal(pred, labels)
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    loss=tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logit))
+    return loss,accuracy,pred
+
+def train(dataset="UCF-101"):
+    # Get the sets of images and labels for training, validation, and
+    # Tell TensorFlow that the model will be built into the default Graph.
+    dataset=dataset.lower()
+    # Create model directory
+    if not os.path.exists(model_save_dir):
+        os.makedirs(model_save_dir)
+    # use_pretrained_model = True
+    # model_filename = "./sports1m_finetuning_ucf101.model"
+
+
+    with tf.Graph().as_default():
+        images_placeholder, labels_placeholder = placeholder_inputs()
+        lr = tf.placeholder(tf.float32)
+        c3d = C3D.C3D(num_class=101)
+
+        logit = c3d.model(images_placeholder, batch_size=BATCH_SIZE)
+        loss,accuracy,pred=get_loss_accuracy(logit,labels_placeholder)
+
+        train_op = tf.train.AdamOptimizer(lr).minimize(loss)
+        train_data = dataReader.dataReader("data/"+dataset+"_train_list.txt", batch_size=BATCH_SIZE)
+        test_data = dataReader.dataReader("data/"+dataset+"_test_list.txt", batch_size=BATCH_SIZE)
+        init = tf.global_variables_initializer()
+        sess = tf.Session(
+            config=tf.ConfigProto(allow_soft_placement=True)
+        )
+        sess.run(init)
+        saver = tf.train.Saver()
+        # ckpt=tf.train.get_checkpoint_state(model_save_dir)
+        # if ckpt and ckpt.model_checkpoint_path:
+        #     print(ckpt.model_checkpoint_path)
+        #     saver.restore(sess, ckpt.model_checkpoint_path)
+        # Create a session for running Ops on the Graph.
+
+        learning_rate=0.003
+        for step in range(FLAGS.max_steps):
+            if step!=0 and step%2e+5 ==0:learning_rate/=10
+            # start_time = time.time()
+            train_images, train_labels = train_data.get_next_batch()
+
+            sess.run(train_op, feed_dict={
+                images_placeholder: train_images,
+                labels_placeholder: train_labels,
+                lr:learning_rate
+            })
+            # duration = time.time() - start_time
+            # print('Step %d: %.3f sec' % (step, duration))
+
+            # Save a checkpoint and evaluate the model periodically.
+            if (step) % 10 == 0 or (step + 1) == FLAGS.max_steps:
+                print('step %d Train'%(step))
+                acc, _loss, _pred,_logit = sess.run(
+                    [accuracy, loss, pred,logit],
+                    feed_dict={images_placeholder: train_images,
+                               labels_placeholder: train_labels,
+                               lr: learning_rate
+                               })
+
+                print (_logit)
+                # print (_pred)
+                print ("accuracy: " + "{:.5f}".format(acc) + " loss:" + "{:.5f}".format(_loss))
+            if (step) % 100 == 0 or (step + 1) == FLAGS.max_steps:
+                print('step %d Test'%(step))
+                test_images, test_labels = test_data.get_next_batch()
+                _loss, acc = sess.run(
+                    [loss, accuracy],
+                    feed_dict={
+                        images_placeholder: test_images,
+                        labels_placeholder: test_labels,
+                        lr: learning_rate
+                    })
+                print ("accuracy: " + "{:.5f}".format(acc) + " loss:" + "{:.5f}".format(_loss))
+        saver.save(sess,model_save_dir+'/'+dataset+'.model')
+
+    print("done")
+
+def test(dataset="UCF-101"):
+    dataset=dataset.lower()
+    with tf.Graph().as_default():
+        images_placeholder, labels_placeholder = placeholder_inputs()
+        c3d = C3D.C3D(num_class=101)
+
+        logit = c3d.model(images_placeholder, batch_size=BATCH_SIZE)
+        correct_pred = tf.reduce_sum(tf.cast(tf.equal(tf.argmax(logit, 1), labels_placeholder),tf.int16))
+        print(correct_pred)
+        sess = tf.Session()
+
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
+        saver.restore(sess, model_save_dir + '/'+dataset+'.model')
+        test_data = dataReader.dataReader("data/"+dataset+"_test_list.txt", batch_size=BATCH_SIZE)
+        finished=False
+        cnt=0
+        total=0
+        while(not finished):
+            test_images, test_labels = test_data.get_next_batch()
+            finished=test_data.is_finished()
+            _correct_pred = sess.run(
+                [correct_pred],
+                feed_dict={
+                    images_placeholder: test_images,
+                    labels_placeholder: test_labels
+                })
+            total+=test_images.shape[0]
+            cnt+=_correct_pred[0]
+        print(cnt,total,cnt/float(total))
+
+
+def main(_):
+    train()
+    # test()
+
+
+if __name__ == '__main__':
+    tf.app.run()
